e5d104c87a7074e4bf83c3f186ee7d221022cad1
Bart v. Eijkelenburg <bart.vaneijkelenburg@hu.nl>
2023-04-20 10:30:58 +0200

    practicum 1.1 en 1.2

--- p1-client-server/src/main/java/clientserver/Client.java
+++ p1-client-server/src/main/java/clientserver/Client.java
@@ -1,4 +1,29 @@
 package clientserver;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
+    public static void main(String[] args) {
+        try {
+            Socket clientSocket = new Socket("localhost", 4711);
+            OutputStream os = clientSocket.getOutputStream();
+
+            Scanner keyboardScanner = new Scanner(System.in);
+            String input = keyboardScanner.nextLine();
+
+            while (!input.isEmpty()) {
+                os.write((input + "\n").getBytes());
+                input = keyboardScanner.nextLine();
+            }
+
+            os.close();
+
+        } catch (IOException ioe) {
+            System.out.println("Er ging iets mis bij het versturen van een bericht!");
+            ioe.printStackTrace();
+        }
+    }
 }
--- p1-client-server/src/main/java/clientserver/Server.java
+++ p1-client-server/src/main/java/clientserver/Server.java
@@ -1,4 +1,39 @@
 package clientserver;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Server {
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket serverSocket = new ServerSocket(4711);
+        while (true) {
+            Socket mySocket = serverSocket.accept();
+
+            InputStream is = mySocket.getInputStream();
+
+            Scanner sc = new Scanner(is);
+            while (sc.hasNextLine()) {
+                String line = sc.nextLine();
+                System.out.println(line);
+                if (line.equals("")) {
+                    break;
+                }
+            }
+
+            OutputStream os = mySocket.getOutputStream();
+            os.write("HTTP/1.1 200 OK\r\n".getBytes());
+            os.write("\r\n".getBytes());
+            os.write("<h1>It works!</h1>".getBytes());
+
+            os.close();
+            is.close();
+            sc.close();
+            mySocket.close();
+        }
+    }
 }
