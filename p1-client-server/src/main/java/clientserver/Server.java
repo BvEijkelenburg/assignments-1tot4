76e38bb43baaebfd7e3a467aa663ace442abff6b
Bart v. Eijkelenburg <bart.vaneijkelenburg@hu.nl>
2023-05-11 16:07:32 +0200

    practicum 1.3

--- p1-client-server/src/main/java/clientserver/Server.java
+++ p1-client-server/src/main/java/clientserver/Server.java
@@ -5,6 +5,8 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Scanner;
 
 public class Server {
@@ -15,10 +17,15 @@ public class Server {
             Socket mySocket = serverSocket.accept();
 
             InputStream is = mySocket.getInputStream();
-
+            Path file = null;
             Scanner sc = new Scanner(is);
             while (sc.hasNextLine()) {
                 String line = sc.nextLine();
+
+                if (line.startsWith("GET")) {
+                    String resource = line.substring(4, line.lastIndexOf(" "));
+                    file = Path.of("files" + resource);
+                }
                 System.out.println(line);
                 if (line.equals("")) {
                     break;
@@ -28,7 +35,12 @@ public class Server {
             OutputStream os = mySocket.getOutputStream();
             os.write("HTTP/1.1 200 OK\r\n".getBytes());
             os.write("\r\n".getBytes());
-            os.write("<h1>It works!</h1>".getBytes());
+
+            if (file != null && Files.exists(file)) {
+                Files.copy(file, os);
+            } else {
+                os.write("<h1>File not found!</h1>".getBytes());
+            }
 
             os.close();
             is.close();
